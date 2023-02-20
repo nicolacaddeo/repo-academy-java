9854b5d14f185974442bcba71473a45fd5841824
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-02-20 12:57:23 +0100

    commit

--- Fibonacci.java
+++ Fibonacci.java
@@ -9,15 +9,15 @@ public class Fibonacci {
         int userInput = scannerInput.nextInt();
         System.out.println(" " + num2);
 
-        for (int i = 1; i < userInput; i++) { // il ciclo stampa solo i numeri dispari 
+        for (int i = 1; i < userInput; i++) { // il ciclo stampa solo i numeri dispari
             num3 = num1 + num2;
             int even = num3 % 2;
-            if (even != 0) { 
+            if (even != 0) {
                 System.out.println(" " + num3);
 
             }
             num1 = num2;
-            num2 = num3;
+            num2 = num3; //commento
         }
 
         scannerInput.close();
