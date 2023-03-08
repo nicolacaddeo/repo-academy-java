c96d31730bf3a93e9997e48a7e2f703ba3b5964d
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-08 12:51:06 +0100

    Update ConnectionWorld.java

--- SQL_JDBC/jdbc_fourth_week/ConnectionWorld.java
+++ SQL_JDBC/jdbc_fourth_week/ConnectionWorld.java
@@ -25,20 +25,24 @@ public class ConnectionWorld {
             Connection conn;
             conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             String query = "SELECT * FROM citta_ita";
-            // Statement stm = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-            // ResultSet.CLOSE_CURSORS_AT_COMMIT);
             Statement stm = conn.createStatement();
             ResultSet rs = stm.executeQuery(query);
+
+            // * statement per recuperare il numero dei record
+            Statement stm1 = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs1 = stm1.executeQuery("SELECT * FROM citta_ita");
+
             // * MetaData -------
             ResultSetMetaData rsMetaData = rs.getMetaData();
+            ResultSetMetaData rs1MetaData = rs.getMetaData();
             // * recupero il numero delle colonne
             int columsNum = rsMetaData.getColumnCount();
 
             // * sposto il cursore per andare all'ultima riga
-            // rs.last();
+            rs1.last();
 
             // * determino la posizione dell'ultima riga
-            // int recordsNumber = rs.getRow();
+            int recordsNumber = rs1.getRow();
 
             while (rs.next()) {
                 for (int i = 1; i <= columsNum; i++) {
@@ -47,7 +51,7 @@ public class ConnectionWorld {
                 }
                 System.out.println("");
             }
-            // System.out.println("Numero di records: " + recordsNumber);
+            System.out.println("Numero di records: " + recordsNumber);
             return conn;
 
         } catch (Exception e) {
@@ -56,9 +60,4 @@ public class ConnectionWorld {
         }
     }
 
-    public void GetRowNumber(String query, ResultSet rs) {
-        // rs.last();
-        // return rs.getRow();
-
-    }
 }
