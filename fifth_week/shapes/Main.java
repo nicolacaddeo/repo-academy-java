d4009f6a81840e1c0f91c40d7f21c5de630ce6ef
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-14 17:54:27 +0100

    aggiunta l'opzione per aggiunta forma tramite input

--- fifth_week/shapes/Main.java
+++ fifth_week/shapes/Main.java
@@ -1,30 +1,72 @@
 package shapes;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        ShapeManager shapeManager = new ShapeManager();
 
-        try {
-            Circle cerchio1 = new Circle(getRandomNumber());
-            Circle cerchio2 = new Circle(getRandomNumber());
+        boolean exit = false;
 
-            Triangle triangolo1 = new Triangle(getRandomNumber(), getRandomNumber(), getRandomNumber());
-            Triangle triangolo2 = new Triangle(getRandomNumber(), getRandomNumber(), getRandomNumber());
+        while (!exit) {
+            int userChoice = getInput(
+                    "Seleziona un'opzione:\n[1] Aggiungi forma\n[2] Calcola area totale\n[3] Calcola perimetro totale\n[4] Esci");
+            switch (userChoice) {
+                case 1:
+                    int shapeChoice = getInput("[1] Cerchio\n[2] Triangolo");
+                    if (shapeChoice == 1) {
+                        shapeManager.addShape(new Circle(getRandomNumber()));
+                        System.out.println("Cerchio aggiunto correttamente");
 
-            ShapeManager shapeManager = new ShapeManager();
+                    } else if (shapeChoice == 2) {
+                        shapeManager.addShape(new Triangle(getRandomNumber(), getRandomNumber(), getRandomNumber()));
+                        System.out.println("Triangolo aggiunto correttamente");
 
-            shapeManager.addShape(cerchio1);
-            shapeManager.addShape(cerchio2);
-            shapeManager.addShape(triangolo1);
-            shapeManager.addShape(triangolo2);
-
-            // * Test dei metodi
-            shapeManager.getShapes();
-            shapeManager.getTotalArea();
-            shapeManager.getTotalPerimeter();
-
-        } catch (Exception e) {
-            System.out.println("Errore: " + e);
+                    } else {
+                        System.out.println("Opzione non disponibile");
+                    }
+                    break;
+                case 2:
+                    shapeManager.getTotalArea();
+                    break;
+                case 3:
+                    shapeManager.getTotalPerimeter();
+                    break;
+                case 4:
+                    System.out.println("Ciao!");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Opzione non disponibile");
+            }
         }
+        /*
+         * Circle cerchio1 = new Circle(getRandomNumber());
+         * Circle cerchio2 = new Circle(getRandomNumber());
+         * 
+         * Triangle triangolo1 = new Triangle(getRandomNumber(), getRandomNumber(),
+         * getRandomNumber());
+         * Triangle triangolo2 = new Triangle(getRandomNumber(), getRandomNumber(),
+         * getRandomNumber());
+         * 
+         * shapeManager.addShape(cerchio1);
+         * shapeManager.addShape(cerchio2);
+         * shapeManager.addShape(triangolo1);
+         * shapeManager.addShape(triangolo2);
+         * 
+         * // * Test dei metodi
+         * shapeManager.getShapes();
+         * shapeManager.getTotalArea();
+         * shapeManager.getTotalPerimeter();
+         */
+    }
+
+    public static int getInput(String string) {
+        Scanner intScanner = new Scanner(System.in);
+        System.out.println(string);
+        int userChoice = intScanner.nextInt();
+
+        return userChoice;
     }
 
     public static int getRandomNumber() {
